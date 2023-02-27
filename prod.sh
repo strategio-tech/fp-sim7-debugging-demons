acfb29267edd5cdabbc2f42d8ea94aa720309077
Eddie Espinosa <espinosa.edilson@gmail.com>
2023-02-27 16:51:26 -0500

    automated commit

--- prod.sh
+++ prod.sh
@@ -1,6 +1,8 @@
 #! /bin/bash
 
-pipenv run build
+python3 -m pipenv run pip freeze > requirements.txt
+python3 -m pipenv run pip install --requirement requirements.txt
+zip -r scribble-ai.zip . -x "*.git*" "*.venv*" "*.vscode*" "*.idea*" "*.DS_Store*"
 
 git add .
 git commit -m 'automated commit'
