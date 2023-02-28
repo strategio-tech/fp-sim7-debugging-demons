8e98f899a7d8016ee262515182cadd35f11f479c
Eddie Espinosa <espinosa.edilson@gmail.com>
2023-02-28 10:04:03 -0500

    automated commit

--- build.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/bash
-python3 -m pipenv run pip freeze > requirements.txt
-python3 -m pipenv run pip install --requirement requirements.txt
-zip -r scribble-ai.zip . -x "*.git*" "*.venv*" "*.vscode*" "*.idea*" "*.DS_Store*"
--- init.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-nginx -s stop
-cd /var/app/current/
-pipenv run pip install --requirement requirements.txt
-cd api/ && pipenv run gunicorn -b 0.0.0.0:80 main:app
