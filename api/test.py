a8cfdbf5cad99472c247456352fe724570b43b2e
peny6jo <penelopeseijo@gmail.com>
2023-03-01 13:24:09 -0500

    Test differents code response for signup endpoint

--- api/test.py
+++ api/test.py
@@ -66,10 +66,33 @@ def test_handlePrompt_invalid_token(client):
     # assert response.json["name"] == "testuser"
     # assert response.json["token"] is not None
     # assert len(response.json["promptHistory"]) >= 0 
+
+
+def test_signup_new_user(client):
+    # Prepare data for the request
+    data = {
+        "name": "Test User",
+        "user": "testUser",
+        "password": "mysecretpassword",
+        "admin_password": ADMIN_PASSWORD
+    }
+   
+    # Make a POST request to the signup endpoint
+    response = client.post('/signup', data=json.dumps(data), headers=headers)
+
+    # Check that the response status code is 200
+    assert_that(response.status_code).is_equal_to(requests.codes.ok)
     
+    # Check that the response contains the correct user data
+    response_data = json.loads(response.get_data(as_text=True))
+    assert_that(response_data["name"]) == "testuser"
+    assert_that(response_data["token"]).is_not_none
+    assert_that(len(response_data["promptHistory"])).is_greater_than(0)
+   
+      
 def test_signup_existing_user(client):
 
-    # Prepare duplicatedata for another request with the same username
+    # Prepare duplicate data for another request with the same username
     data_duplicate = {
         "name": "Other Test User",
         "user": "testUser",
@@ -77,22 +100,19 @@ def test_signup_existing_user(client):
         "admin_password": ADMIN_PASSWORD
     }
   
-
     # Make another POST request to the signup endpoint with the same username
     response_duplicate = client.post('/signup', data=json.dumps(data_duplicate), headers=headers)
    
-    # Check that the response status code is 400 because the user already exists and has the correct message
+    # Check that the response status code is 400 because the user already 
     assert_that(response_duplicate.status_code).is_equal_to(requests.codes.bad_request)
     
     # Check that the response has the correct message
     response_duplicate_data = json.loads(response_duplicate.get_data(as_text=True))
     assert_that(response_duplicate_data["message"]).is_equal_to("This user already exists.")
-    
-    # assert response_duplicate_data.status_code == 400
-    # assert response_duplicate.json["message"] == "This user already exists."
+  
 
 def test_signup_invalid_admin_password(client):
-    # Mock data for a request with an invalid admin password
+    # Prepare data for a request with an invalid admin password
     data = {
         "name": "Other Test User2",
         "user": "testUser2",
@@ -101,14 +121,12 @@ def test_signup_invalid_admin_password(client):
     }   
     
     # Make a POST request to the signup endpoint with the wrong password
-    
     response= client.post('/signup', data=json.dumps(data), headers=headers)
 
-    # Check that the response status code is 401 because the password is incorrect and has the correct message
+    # Check that the response status code is 401 because the password is incorrect
     assert_that(response.status_code).is_equal_to(requests.codes.unauthorized)
     
-   
-    # Check that the response data has the correct message
+   # Check that the response data has the correct message
     response_data = json.loads(response.get_data(as_text=True))
     assert_that(response_data["message"]).is_equal_to("Invalid admin password")
     
